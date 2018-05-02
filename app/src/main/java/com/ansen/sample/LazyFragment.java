ec1a412189746a6547e7ce7a8ae919e257938b2b
ansen360 <ansen360@126.com>
2018-05-02 17:16:03 +0800

    add lazy view

--- app/src/main/java/com/ansen/sample/LazyFragment.java
+++ app/src/main/java/com/ansen/sample/LazyFragment.java
@@ -7,26 +7,33 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class LazyFragment extends Fragment {
-    String isLoad = "没有被加载";
+
+    private boolean mIsInit;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        //开启线程，加载数据成功
-        String isLoad = "被加载了";
-
+        mIsInit = true; // View 控件的初始化
+        isLoadData();   // 这里还需满足条件1: 视图对用户可见
         return super.onCreateView(inflater, container, savedInstanceState);
 
 
     }
 
+    /**
+     * 这个方法是通知Fragment的UI是否可见，当参数isVisibleToUser为true的时候，fragment的UI是可见的，为false的时候为不可见
+     */
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        if (isVisibleToUser) { //当然使用getUserVisibleHint()代替isVisibleToUser也可以，看上面的源码就知道，getUserVisibleHint()获取到的就是isVisibleToUser
-            /** 这里仅仅只是个伪代码*/
-            if (!isLoad.equals("被加载了")) {
-                //加载数据
+        isLoadData();   // 还需满足条件2: 视图已经初始化
+    }
+
+    private void isLoadData() {
+        if (getUserVisibleHint()) {
+            if (mIsInit) {
+                // 满足以上两个条件才加载数据
             }
+
         }
     }
 }
