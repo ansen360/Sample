e8ae3a3a392032c8d0ebeaa2c88b026115361330
ansen360 <ansen360@126.com>
2018-06-16 17:28:27 +0800

    constraintlayout

--- app/src/main/java/com/ansen/sample/MainActivity.java
+++ app/src/main/java/com/ansen/sample/MainActivity.java
@@ -1,14 +1,27 @@
 package com.ansen.sample;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.FrameMetrics;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_traditional);
+//        setContentView(R.layout.activity_constraintlayout);
+        getWindow().addOnFrameMetricsAvailableListener(new Window.OnFrameMetricsAvailableListener() {
+            @Override
+            public void onFrameMetricsAvailable(Window window, FrameMetrics frameMetrics, int dropCountSinceLastInvocation) {
+//                // Layout measure duration in nanoseconds
+                long layoutMeasureDurationNs = frameMetrics.getMetric(FrameMetrics.LAYOUT_MEASURE_DURATION);
+                Log.d("ansen", "layoutMeasureDurationNs: " + layoutMeasureDurationNs);
+            }
+        }, new Handler());
 
     }
 
